1492c92fbb243b3c8792a663762a692e250e8213
stajkowski <stajkowski100@gmail.com>
2015-08-09 21:36:02 -0700

    Revised for LXC install

--- cloudstack-lxc-host.sh
+++ cloudstack-lxc-host.sh
@@ -21,11 +21,21 @@ service ntp restart
 
 
 #Install Cloudstack Agent
-apt-get install lxc cloudstack-agent -y
+apt-get install lxc kvm qemu qemu-kvm cloudstack-agent -y
 
 
 #Set Agent Configuration
 sed -i "s/hypervisor.type=.*/hypervisor.type=lxc/g" /etc/cloudstack/agent/agent.properties
+UUID=`uuidgen`
+sed -i "s/guid=.*/guid=$UUID/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/local.storage.uuid=.*/local.storage.uuid=$UUID/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/#public.network.device=cloudbr0/public.network.device=cloudbr0/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/#private.network.device=cloudbr1/private.network.device=cloudbr1/g" /etc/cloudstack/agent/agent.properties
+
+sed -i "s/zone=.*/zone=SKILAB/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/pod=.*/pod=SKILAB/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/cluster=.*/cluster=SKILAB_LXC/g" /etc/cloudstack/agent/agent.properties
+sed -i "s/host=.*/host=cloudstack.skilab.com/g" /etc/cloudstack/agent/agent.properties
 
 #Configure libvirt
 sed -i "s/#listen_tls.*/listen_tls = 0/g" /etc/libvirt/libvirtd.conf
@@ -37,7 +47,7 @@ sed -i "s/#mdns_adv.*/mdns_adv = 0/g" /etc/libvirt/libvirtd.conf
 sed -i "s/libvirtd_opts.*/libvirtd_opts=\"-d -l\"/g" /etc/default/libvirt-bin
 
 #Configure VNC
-sed -i "s/#vnc_listen.*/vnc_listen = "0.0.0.0"/g" /etc/libvirt/qemu.conf
+sed -i "s/#vnc_listen.*/vnc_listen = \"0.0.0.0\"/g" /etc/libvirt/qemu.conf
 
 service libvirt-bin restart
 
@@ -46,3 +56,8 @@ ln -s /etc/apparmor.d/usr.sbin.libvirtd /etc/apparmor.d/disable/
 ln -s /etc/apparmor.d/usr.lib.libvirt.virt-aa-helper /etc/apparmor.d/disable/
 apparmor_parser -R /etc/apparmor.d/usr.sbin.libvirtd
 apparmor_parser -R /etc/apparmor.d/usr.lib.libvirt.virt-aa-helper
+
+update-rc.d cloudstack-agent defaults
+update-rc.d libvirt-bin defaults
+service libvirt-bin restart
+service cloudstack-agent restart
